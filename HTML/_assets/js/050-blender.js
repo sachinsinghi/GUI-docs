d7090422322c3762dea6803dc605b18ec48fbc87
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2015-08-06 16:10:13 +1000

    added blender front script

--- HTML/_assets/js/050-blender.js
+++ HTML/_assets/js/050-blender.js
@@ -51,11 +51,25 @@
 	module.init = function() {
 		App.debugging( 'Blender: Initiating', 'report' );
 
-		App.blender.load();
-
-
-		if( $('.js-blender').length ) {
+		if( $('.js-module-version').length ) {
 			App.debugging( 'Blender: Found instance', 'report' );
+
+
+			var baseVersion = $('.js-blender-size').attr('data-base-version');
+			var baseSize = parseInt( $('.js-blender-size').attr('data-base-size') );
+
+			App.blender.save( '_base', baseVersion, baseSize );
+
+
+			//////////////////////////////////////////////////| RESOLVING HASH
+			if( window.location.hash ) {
+				App.blender.hash();
+			}
+
+
+			//////////////////////////////////////////////////| LOAD FROM LOCAL STORAGE
+			App.blender.load();
+
 
 			//////////////////////////////////////////////////| SELECT HAS CHANGED
 			$('.js-blender-version').on('change', function() {
@@ -74,14 +88,16 @@
 
 					var $select = $wrapper.find('.js-blender-version');
 					var moduleName = $select.attr('id');
-					var version = $select.find('option:selected').val();
+					var $version = $select.find('option:selected');
+					var version = $version.val();
+					var size = $version.attr('data-size');
 					var dependencies = $this.find(':checked').attr('data-dependencies');
 
 					if( dependencies ) {
 						handleDep( dependencies );
 					}
 
-					App.blender.save( moduleName, version );
+					App.blender.save( moduleName, version, size );
 				}
 
 				$wrapper.find('.js-blender-newer').remove(); //remove warnings
@@ -102,27 +118,24 @@
 				var $select = $wrapper.find('.js-blender-version');
 				var dependencies = $select.find(':checked').attr('data-dependencies');
 				var moduleName = $select.attr('id');
+				var $version = $select.find('option:selected');
+				var size = $version.attr('data-size');
 
 				if( _isChecked ) {
-					var version = $select.find('option:selected').val();
+					var version = $version.val();
 
 					if( dependencies ) {
 						handleDep( dependencies );
 					}
 
-					App.blender.save( moduleName, version );
+					App.blender.save( moduleName, version, size );
 				}
 				else {
-					App.blender.remove( moduleName );
+					App.blender.remove( moduleName, size );
 				}
 
 			});
 
-
-			//////////////////////////////////////////////////| RESOLVING HASH
-			if( window.location.hash ) {
-				App.blender.hash();
-			}
 		}
 	};
 
@@ -133,45 +146,67 @@
 	module.hash = function() {
 		App.debugging( 'Blender: Parsing hash', 'report' );
 
-		var hash = window.location.hash.split('/');
-		delete hash[0]; //remove empty item
+		if( $('.js-blender').length ) {
+			App.debugging( 'Blender: Parsing hash: found blender form', 'report' );
 
-		for(var i = hash.length - 1; i >= 0; i--) {
-			if( hash[i] !== undefined ) {
+			var hash = window.location.hash.split('/');
+			delete hash[0]; //remove empty item
 
-				var module = hash[i].split(':');
-				var $version = $('.js-blender-version[name="module-' + module[0] + '"]');
+			if( hash[1] !== '' ) {
+				App.debugging( 'Blender: Parsing hash: clearing localStorage and overwriting with hash', 'report' );
 
-				$version
-					.val( module[1] ) //select version
-					.trigger('change'); //executing event handler
+				store.clear();
 			}
-		};
+
+			for(var i = hash.length - 1; i >= 0; i--) {
+				if( hash[i] !== undefined && hash[i] !== '' ) {
+					App.debugging( 'Blender: Parsing hash: parsing module "' + hash[i] + '"', 'report' );
+
+					var module = hash[i].split(':');
+					var size = $('.js-blender-version option.js-blender-' + module[0] + '[data-version="' + module[1] + '"]').attr('data-size');
+
+					App.blender.save( module[0], module[1], size );
+				}
+			};
+		}
 	};
 
 
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
 	// Save to localStorage
 	//
-	// @param  module   string  The ID of the module to be saved
-	// @param  version  string  The version string for that module to be saved
+	// @param  module   string   The ID of the module to be saved
+	// @param  version  string   The version string for that module to be saved
+	// @param  size     integer  The size of this version to be saved
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
-	module.save = function( moduleName, version ) {
-		App.debugging( 'Blender: Saving module ' + moduleName + ':' + version + ' to LocalStorage', 'report' );
+	module.save = function( moduleName, version, size ) {
+		if( moduleName && version ) {
+			App.debugging( 'Blender: Saving module ' + moduleName + ':' + version + '(' + size + 'kb) to LocalStorage', 'report' );
 
-		store.set(moduleName, version);
+			store.set( App.PREFIX + moduleName, {
+				'version': version,
+				'size': parseInt( size ) }
+			);
+
+			App.blender.update();
+		}
 	};
 
 
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
 	// Remove from localStorage
 	//
-	// @param  module   string  The ID of the module to be saved
+	// @param  module   string   The ID of the module to be saved
+	// @param  size     integer  The size of this version to be saved
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------
-	module.remove = function( moduleName ) {
-		App.debugging( 'Blender: Removing module ' + moduleName + ' from LocalStorage', 'report' );
+	module.remove = function( moduleName, size ) {
+		if( moduleName ) {
+			App.debugging( 'Blender: Removing module ' + moduleName + '(' + size + 'kb) from LocalStorage', 'report' );
 
-		store.remove(moduleName);
+			store.remove( App.PREFIX + moduleName );
+
+			App.blender.update();
+		}
 	};
 
 
@@ -181,17 +216,43 @@
 	module.load = function() {
 		App.debugging( 'Blender: Loading modules from LocalStorage', 'report' );
 
-		store.forEach(function( moduleName, version ) {
-			App.debugging( 'Blender: Loading modules: found module "' + moduleName + '" in "' + version + '"', 'report' );
+		store.forEach(function( moduleName, options ) {
+			App.debugging( 'Blender: Loading modules: loading module "' + moduleName + '" version "' + options.version + '"(' + options.size + 'kb)', 'report' );
 
-			var $tick = $( '#tick-' + moduleName );
-			var $select = $( '#' + moduleName );
+			if( moduleName.substring(0, App.PREFIX.length) === App.PREFIX ) {
+				moduleName = moduleName.substring( App.PREFIX.length );
+				var $tick = $( '#tick-' + moduleName );
+				var $select = $( '#' + moduleName );
 
-			if( $tick.length ) {
-				$select.find('option[value="' + version + '"]').prop('selected', true);
-				$tick.prop('checked', true);
+				if( $tick.length ) {
+					$select.find('option[value="' + options.version + '"]').prop('selected', true);
+					$tick.prop('checked', true);
+				}
 			}
-		})
+		});
+
+		App.blender.update();
+	};
+
+
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// Update blender cart count
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------
+	module.update = function() {
+		App.debugging( 'Blender: Updating blender cart count', 'report' );
+
+		var count = 0;
+		var size = 0;
+
+		store.forEach(function( moduleName, options ) {
+			if( moduleName.substring(0, App.PREFIX.length) === App.PREFIX ) {
+				size += parseInt( options.size );
+				count++;
+			}
+		});
+
+		$('.js-blender-count').text( ( count - 1 ) );
+		$('.js-blender-size').text( size );
 	};
 
 
